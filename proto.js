c841fe89625e03e1bba6cacfc006030484a6631f
xSplit <rophp@live.it>
2014-06-08 04:56:28 +0200

    Update proto.js

--- proto.js
+++ proto.js
@@ -1,4 +1,4 @@
-/* JS prototype helper lib by xSplit */
+/* JS prototype helper lib by xSplit (my old one from pastebin) */
 Object.prototype.keys = function(){ return Object.keys(this); }
 Object.prototype.name = function(){ return this.constructor.name; }
 Object.prototype.obj = function(){ return Object.prototype.toString.call(this); }
